b993780a3c80146d4a1f403ebfd6c38b7cc4869b
Victor / Dante <125316907+Dantedevil10@users.noreply.github.com>
2024-02-26 15:54:01 -0300

    Add files via upload

---
+++ RandomNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+ class Program
+{
+    public static void Main(string[] args)
+    {
+        Random random = new Random();
+
+        int a1 = random.Next(1,10);
+        int a2 = random.Next(1, 10);
+        int a3 = random.Next(1, 10);
+        //double a = random.NextDouble(); 
+
+        Console.WriteLine(a1);
+        Console.WriteLine(a2);
+        Console.WriteLine(a3);
+
+
+    }
+}
---
+++ UsersInput/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+ class Program
+{
+    public static void Main(string[] args)
+    {
+            
+        Console.WriteLine("Mano, Tu é Gay MANO ? ");
+        string resposta = Console.ReadLine();
+
+        if(resposta == "")
+        {
+            Console.WriteLine("Dexo Vaziu Né HAMMMMM");
+        }
+
+        if(resposta != "nao" && resposta != "não" )
+        {
+            Console.WriteLine("ANnnnn");
+        }
+        else
+        {
+            Console.WriteLine("um Sei");
+        }
+
+        Console.WriteLine("Tu Gosta Né?");
+        resposta = Console.ReadLine();
+
+        if (resposta == "")
+        {
+            Console.WriteLine(" Vaziu DINOVO To achando Que Tu é eim");
+        }
+
+        if (resposta != "nao" && resposta != "não")
+        {
+            Console.WriteLine("ELE GOOOOOSSSSTA");
+        }
+        else
+        {
+            Console.WriteLine("IIIIIIIIHHAAAAAAAAAA");
+        }
+
+        Console.WriteLine("Tu Tem quantos ANux? ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+        
+
+        Console.WriteLine("Esse Carinha teim: " + (idade));
+    }
+}
