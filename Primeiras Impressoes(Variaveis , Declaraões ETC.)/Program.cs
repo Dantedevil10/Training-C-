b5980799474828314abe55bbfb207a2558173a8f
Victor / Dante <125316907+Dantedevil10@users.noreply.github.com>
2024-02-26 11:06:19 -0300

    Add files via upload

---
+++ Primeiras Impressoes(Variaveis , Declaraões ETC.)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+ class Program
+{
+    public static void Main(string[] args)
+    {
+        string myass = "The Problem is => x * 2 = 10\nX equals to: ";
+        int x = 5;
+        int result = x * 2 ;
+        bool You = false;
+        string gay;
+        if(You == true)
+        {
+            gay = "Gay";
+        }
+        else
+        {
+            gay = "Stupid";
+        }
+        Console.WriteLine(myass + x +"\nYou are: "+gay+"\nand Your Ass are "+ result + "\n");
+    }
+}
