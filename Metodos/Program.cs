dff93e2fcc64b6cc6a8f45cc82972f8e5b43bfdc
Victor / Dante <125316907+Dantedevil10@users.noreply.github.com>
2024-02-28 14:15:56 -0300

    Add files via upload

---
+++ Metodos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+ class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //Metodos => Eu Resumo Que Metodos São Funções com um Nome Diferente.
+
+        int idade = 21;
+        String nome = "Tua Mãe";
+
+        cantandoprabains(nome,idade);
+    }
+    static void cantandoprabains(string teunome, int tuaidade)
+    {
+        Console.WriteLine("Parabeins Pra tu Imbencil");
+        Console.WriteLine("tu teins " + tuaidade + " Anus de idade");
+        Console.WriteLine("Parabeinsx pra " + teunome);
+    }
+}
