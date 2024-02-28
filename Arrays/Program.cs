08495e4b0086b7363909d64dde76112664d68304
Victor / Dante <125316907+Dantedevil10@users.noreply.github.com>
2024-02-28 13:57:21 -0300

    Add files via upload

---
+++ Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+ class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //arrays 
+
+        // (String[] cars = new string[4]; isso define um valor fixo de arrays)
+
+
+        String[] cars = { "Mustang", "Camaro", "Ferrari" ,"Corvette"};
+
+        //Console.WriteLine(cars);  //isso não vai fazer o app mostrar todos os arrays
+                                    //isso vai dar um System.String[] ;ou seja é um array de String
+                                
+
+        //pra resolver esse problema de mostrar todos os arrays Pode-se criar um loop for
+
+        for (int i = 0;i < cars.Length; i++)
+        {
+            Console.WriteLine(cars[i]);
+        }
+        //dessa maneira todos os objetos do array serão mostrados
+    }
+}
