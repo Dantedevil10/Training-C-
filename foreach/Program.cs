57d9acb9ae3adc33ff5eecc3321e8acf3e0edd4a
Victor / Dante <125316907+Dantedevil10@users.noreply.github.com>
2024-02-28 14:02:49 -0300

    Add files via upload

---
+++ foreach/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+ class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //arrays 
+
+        // (String[] cars = new string[4]; isso define um valor fixo de arrays)
+
+
+        String[] cars = { "Mustang", "Camaro", "Ferrari", "Corvette" };
+
+        //Console.WriteLine(cars);  //isso não vai fazer o app mostrar todos os arrays
+        //isso vai dar um System.String[] ;ou seja é um array de String
+
+
+        // usando Este mesmo problema o Foreach tambem pode ser usado
+        foreach (String car in cars)
+        {
+            Console.WriteLine(car);
+        }
+    }
+}
