5522ec4ba52f6a8bb40152fa66526dc9be846016
Victor / Dante <125316907+Dantedevil10@users.noreply.github.com>
2024-02-28 13:16:46 -0300

    Add files via upload

---
+++ NestedLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+ class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //nested Loops  => Loops dentro de outros Loops
+        //   Uso Variado .mas é bastante usado No sorteamento de algoritmos
+
+        Console.WriteLine("HOw many rows?");
+        int row = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("How many columns?");
+        int columns = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Whats Symbol?");
+        string symbol = Console.ReadLine();
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0;j < columns;j++)
+            {
+                Console.Write(symbol);
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
