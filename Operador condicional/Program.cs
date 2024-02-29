2554d2db37f6264c52758e2014a8bde64eb89d3c
Victor / Dante <125316907+Dantedevil10@users.noreply.github.com>
2024-02-29 15:41:20 -0300

    Add files via upload

---
+++ Operador condicional/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+ class Program
+{
+    public static void Main(string[] args)
+    {
+        //Operador Condicional => é parecido com o JS
+
+        double temperature = 14;
+        string message = (temperature >= 15)  ? "Ta Quente la fora" : "Ta Frio pa !!!!";
+
+        Console.WriteLine(message);
+        //Console.WriteLine((temperature >= 15) ? "Ta Quente la fora" : "Ta Frio pa !!!!");
+        //dessa maneira tbm funciona
+    }
+   
+    
+
+}
