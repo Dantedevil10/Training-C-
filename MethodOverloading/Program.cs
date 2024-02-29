bc8ce7da8b27c375fa147bc81e0fff296d0e9f71
Victor / Dante <125316907+Dantedevil10@users.noreply.github.com>
2024-02-29 15:17:38 -0300

    Add files via upload

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+ class Program
+{
+    public static void Main(string[] args)
+    {
+        //Method overloading => metodos que tem o mesmo nome mas tem parametros diferentes
+
+       int a = 10;
+        int b = 12;
+        int c = 13;
+       
+       
+        Console.WriteLine(multi(a, b ));
+
+        Console.ReadKey();
+    }
+    static int multi(int a, int b)
+    {
+        int z = a * b;
+       return z ; 
+        
+    }
+    static int multi(int a, int b, int c)
+    {
+       
+        return a * b * c;
+
+    }
+
+}
