655986da8719cc933ec8e3823159a93caf5e5cd6
Victor / Dante <125316907+Dantedevil10@users.noreply.github.com>
2024-02-29 15:10:37 -0300

    Add files via upload

---
+++ Return/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+ class Program
+{
+    public static void Main(string[] args)
+    {
+       int a = 10;
+        int b = 12;
+        int result; 
+         
+        Console.WriteLine(multi(a, b));
+
+    }
+    static int multi(int a, int b)
+    {
+        int z = a * b;
+       return z ; 
+        
+    }
+}
