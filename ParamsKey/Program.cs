0e9da2a36702e1d45ecceb73742b4e8471a4591d
Victor / Dante <125316907+Dantedevil10@users.noreply.github.com>
2024-02-29 15:27:08 -0300

    Add files via upload

---
+++ ParamsKey/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+ class Program
+{
+    public static void Main(string[] args)
+    {
+        //Params Keyword => um metodo que contem varios numeros de argumentos
+        //                  o parametro type precisa ser unico - array dimensional
+
+        double total = Math.Floor(check(3.99,5,14,1.50,1.01));
+
+        Console.WriteLine(total);
+
+    }
+    static double check(params double[] prices)
+    {
+        double total = 0;
+        foreach (double price in prices)
+        {
+            total += price;
+        }
+        return total;
+    }
+    
+
+}
