d32854fe1b6a9b8da399af8a0657f98a40d06ad0
Victor / Dante <125316907+Dantedevil10@users.noreply.github.com>
2024-03-04 13:34:39 -0300

    Add files via upload

---
+++ ArraydeObjetos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Xml.Schema;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        car[] cars = { new car("Mustang"),new car("Chevette")};
+
+        foreach (car car in cars)
+        {
+            Console.WriteLine(car.model);
+        }
+    }
+}
+class car {
+    public String model;
+    public car(String model) {
+        this.model = model;
+    }
+}
+
