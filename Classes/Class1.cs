eb9c37e571ba5a6519031e213e4314664a8babe3
Victor / Dante <125316907+Dantedevil10@users.noreply.github.com>
2024-03-04 09:54:26 -0300

    Add files via upload

---
+++ Classes/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class EXclasse
+    {
+        public static void mensagem()
+        {
+            Console.WriteLine("Bem-Vindo iSSo Ta Vindo de Uma Outra Classe");
+        }
+        public static void espere()
+        {
+            Console.WriteLine("Essa é a Parte de Espera");
+        }
+        public static void Final()
+        {
+            Console.WriteLine("Essa é a Parte Final");
+            Console.WriteLine($"Quanto é Dois mais Dois? Claro Que é Cinco KAKAKAK \nmintira é {2+2}");
+        }
+
+
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp1;
+using System;
+using System.Timers;
+ class Program
+{
+    public static void Main(string[] args)
+    {
+        //Classe
+        System.Timers.Timer timer = new System.Timers.Timer(2000);  
+        
+        
+        
+
+        EXclasse.mensagem();
+        EXclasse.espere();
+        timer.Start();
+        EXclasse.Final();
+
+
+    }
+   
+    
+
+}
