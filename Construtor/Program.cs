b032c126338496ad14e16bed1f2f5a5a6dc8586f
Victor / Dante <125316907+Dantedevil10@users.noreply.github.com>
2024-03-04 10:21:49 -0300

    Add files via upload

---
+++ Construtor/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+ class Program
+{
+    public static void Main(string[] args)
+    {
+        //Construtor = um metodo especial na classe com o mesmp nome da classe
+        //  que pode ser usado para assinar argumentos em campo quando um objeto é criado
+
+        Games game1 = new Games("God of War",2005,"Playstation2");
+        game1.texto();
+    }
+}
+class Games
+{
+    String nome;
+    int ano;
+    String plataforma;
+
+    public Games(String nome, int ano, String plataforma)
+    {
+        this.nome = nome;
+        this.ano = ano;
+        this.plataforma = plataforma;
+    }
+    public void texto()
+    {
+        Console.WriteLine($"{nome} É Um Game De {ano} Lançado Para {plataforma}");
+    }
+}
